6883160db74f97a26dd2eed0d00c94d9442dfc01
Gustavo Pereira <gustavo.h.pereira13@aluno.senai.br>
2026-02-26 11:09:32 -0300

    guxta

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const btnP1 = document.querySelector('#inputP1');
+const btnP2 = document.querySelector('#inputP2');
+const btnP3 = document.querySelector('#inputP3');
+
+btnP1.addEventListener('click', function (){
+    window.location.href='./pages/projeto01.html'
+})
+
+btnP2.addEventListener('click', function (){
+    window.location.href='./pages/projeto02.html'
+})
+
+btnP3.addEventListener('click', function (){
+    window.location.href='./pages/projeto03.html'
+})
